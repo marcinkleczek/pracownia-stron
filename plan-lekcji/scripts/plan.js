6ba32b56ccb00351743fe0a4af4db36c1f236c31
Marcin Kłeczek <marcin@leanmate.pl>
2022-11-01 16:16:33 +0100

    Dodane przetwarzenie JS i trochę CSS dla RWD.

--- plan-lekcji/scripts/plan.js
+++ plan-lekcji/scripts/plan.js
@@ -47,6 +47,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	document.querySelector('table').remove();
 	document.querySelector('table').appendChild(newCaption);
 
+	let tds = document.querySelectorAll('table.tabela td, table.tabela th');
+	let g = document.createElement('div');
+	g.classList.add('grider');
+	tds.forEach(td => {
+		let tadek = document.createElement('div');
+		let element = td;
+
+		let tr = td.parentNode;
+		let time = tr.querySelector('td:nth-child(2)');
+
+		if (element.firstChild.nodeName === 'SPAN' && element.firstChild.firstChild && element.firstChild.firstChild.nodeName === 'A') {
+			element = element.firstChild;
+			// console.dir(element.firstChild);
+		}
+
+		tadek.innerHTML = '<time>'+ (time ? time.innerHTML: '') + '</time><article>'+element.innerHTML.replaceAll("&nbsp;", "").replaceAll('<br>', "</article><article>") + '</article>';//.replaceAll("<br>", "");
+		tadek.dataset.time = time ? time.innerText : '';
+		tadek.classList.add(td.nodeName === 'TD' ? 'old-td' : 'old-th');
+		g.appendChild(tadek);
+	});
+	document.body.appendChild(g);
+
 	fetch('../scripts/plans.json').then(j => j.json()).then(createSelect);
 });
 
